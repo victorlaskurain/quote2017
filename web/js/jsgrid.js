60ce22e10c79374bc00dc9cf19427f357713144d
Victor Laskurain <victorlaskurain@gmail.com>
2017-11-10 19:31:23 +0100

    Hack to have jsGrid remember the active ordering when filtering.

--- web/js/jsgrid.js
+++ web/js/jsgrid.js
@@ -1,1 +1,13 @@
-define();
+define(function() {
+    /*
+     * Hack to have jsGrid remember the active ordering when
+     * filtering: search resets the paging and the sort order and then
+     * calls loadData. Since we don't want to reset the sort order
+     * when a filter changes, we override search with our own version
+     * which resets only the pager and calls loadData.
+     */
+    jsGrid.Grid.prototype.search = function(filter) {
+        this._resetPager();
+        return this.loadData(filter);
+    };
+});
